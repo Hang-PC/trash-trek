dfedd87b18921d82f6fa32511189a64b18e6c627
Sreesanjay <sreesanjay7592@gmail.com>
2024-05-11 00:30:45 +0530

    contact page added

--- js/contact.js
+++ js/contact.js
@@ -1,26 +1,48 @@
-(function () {
-    emailjs.init({
-        publicKey: 'C9wNpNzGX85GCb-e3',
-        // Do not allow headless browsers
-        blockHeadless: true,
-        limitRate: {
-            // Set the limit rate for the application
-            id: 'app',
-            // Allow 1 request per 10s
-            throttle: 5000,
-        },
-    });
-    var templateParams = {
-        name: 'James',
-        notes: 'Check this out!',
+
+emailjs.init({
+    publicKey: 'C9wNpNzGX85GCb-e3',
+    // Do not allow headless browsers
+    blockHeadless: true,
+    limitRate: {
+        // Set the limit rate for the application
+        id: 'app',
+        // Allow 1 request per 5s
+        throttle: 5000,
+    },
+});
+
+function sendMail(e) {
+    e.preventDefault();
+    console.log("send mail called")
+    const fullname = document.getElementById('user-name')
+    const email = document.getElementById('user-email')
+    const service = document.getElementById('service')
+    const message = document.getElementById('message')
+    const templateParams = {
+        from_name: fullname.value,
+        email_id: email.value,
+        service_name: service.value,
+        message: message.value
     };
 
-    emailjs.send('service_0v8dj2q', 'template_troyvm8', templateParams).then(
+    emailjs.send('service_0v8dj2q', 'template_l4wet1g', templateParams).then(
         (response) => {
             console.log('SUCCESS!', response.status, response.text);
+            Swal.fire({
+                title: "Sent!",
+                text: "Message sent successfully!",
+                icon: "success"
+            }).then(() => {
+                location.assign('/contact.html')
+            })
         },
         (error) => {
             console.log('FAILED...', error);
+            Swal.fire({
+                title: "Error",
+                text: "Something went wrong!",
+                icon: "error"
+            })
         },
     );
-})();
+}
